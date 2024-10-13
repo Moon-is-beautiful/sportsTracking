e0ed848e37f7e64c866edbcc8832ade79e0b5a8d
Jack <jack.zhao72.jz@utexas.edu>
2024-10-13 10:39:55 -0500

    added functionality

--- app.py
+++ app.py
@@ -12,29 +12,49 @@ CORS(app)
 
 # Function that gets triggered at the start of opening up the android app
 # displays initial home page
-@app.route()
+@app.route("/")
 def home():
-
+	return "this is the home page"
 
 
 # Function that gets triggered when API for creating account is called
 # takes information from the front-end and store it in the backend. Will call cipher.py to encrypt password and username
-@app.route()
+@app.route("/createaccount", methods = ["POST"])
 def createAccount():
+	newUserInfo = request.get_json() # parse incoming json request data and return it	
 
+	name  = newUserInfo['name'] # get the information from the key name
+	password = newUserInfo['password'] # get the information from the key password
+	username = newUserInfo['username'] # get the information from the key username
+	
+	#might change so that database uses the cipher.py functions instead of app.py
+	username = cipher.encrypt(username, 3, 2) # encrypt the username
+	password = cipher.encripy(password, 4, 2) # encrypt the password
+
+	database.createUser(name, userID, password) # put the user in our database
+	
+	return 'Done', 201 # postman to check route integrity
 
 
 # Function that triggers when API for logging in gets called
 # authenticates user information by checking if username password are in the database
-@app.route()
+@app.route("/login", methods=["POST"])
 def login():
-
+	userInfo = request.get_json()
+	username = userInfo['loginUsername'] 
+	password = userInfo['loginPassword']
+	
+	username = cipher.encrypt(username, 3, 2)
+	password = cipher.encrypt(username, 4, 2)
+	
+	authentication = database.authenticateLogin(username, password)
+	return jsonify({"authentication": authentication})
 
 
 # Function that will get the routes located in our database and load it into the front-end
 @app.route()
 def getRoutes():
-
+	
 
 
 # Function that will pull more information from a specific route in the database and then return info to the front-end
