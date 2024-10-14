8bb88ad09ffc6ce863fccb625ead74f08fa20fad
Jack <jack.zhao72.jz@utexas.edu>
2024-10-13 22:44:49 -0500

    added test files for individual demonstrations of cipher.py and database.py functionality. Find the files in test folder. Additionally modifed cipher.py by specifiying specific hash method to be sued

--- cipher.py
+++ cipher.py
@@ -3,10 +3,12 @@ import datetime
 
 
 def encrypt_user_id(username):
-    return generate_password_hash(username + datetime.datetime.utcnow().isoformat())
+    return generate_password_hash(username + datetime.datetime.utcnow().isoformat(), method='pbkdf2:sha256')
+
 
 def encrypt_password(password):
-    return generate_password_hash(password)
+    return generate_password_hash(password, method='pbkdf2:sha256')
+
 
 def verify_password(encrypted_password, password):
     return check_password_hash(encrypted_password, password)
